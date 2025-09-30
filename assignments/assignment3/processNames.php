150c879d1f4c827dc2c8f586dc5554d4991209c0
SulaimanAbbott <sulaimanabbott@gmail.com>
2025-09-29 22:58:15 -0400

    updates

--- assignments/assignment3/processNames.php
+++ assignments/assignment3/processNames.php
@@ -4,29 +4,31 @@ function addClearNames() {
     if (isset($_POST['clearNames'])) {
         return "";
     }
+    
     // Add name if addName button is pressed
     if (isset($_POST['addName'])) {
 
         // Empty array to hold names
         $names = [];
 
-        // Check if array is not empty, then populate it
+        // If the textarea already has names, split them into an array
         if (!empty($_POST['namelist'])) {
             $names = explode("\n", $_POST['namelist']);
         }
 
-        // Split the name into two parts
+        // Split the input name into first and last
         [$first, $last] = explode(" ", $_POST['name']);
 
-        // Flip the order and add to array
-        $names[] = "$last, $first";
+        // Flip the order and push into the array
+        array_push($names, "$last, $first");
 
         // Sort names alphabetically
         sort($names);
         
-        // Combine names into single line 
+        // Combine names into one string, each on a new line
         return implode("\n", $names);
     }
 
+    // If no button was pressed, return empty string
     return "";
 }
